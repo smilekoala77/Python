031322c9cfec12d08813923ae6800ffa143ecb5c
smilekoala77 <wangwanjing@gmail.com>
2020-11-17 11:48:49 +0800

    Add - print No. of the disc

--- other/tower_of_hanoi.py
+++ other/tower_of_hanoi.py
@@ -11,17 +11,16 @@ def moveTower(height, fromPole, toPole, withPole):
     """
     if height >= 1:
         moveTower(height - 1, fromPole, withPole, toPole)
-        moveDisk(fromPole, toPole)
+        moveDisk(fromPole, toPole, height)
         moveTower(height - 1, withPole, toPole, fromPole)
 
 
-def moveDisk(fp, tp):
-    print("moving disk from", fp, "to", tp)
-
+def moveDisk(fp, tp, count):
+    print("moving No.", count, " disk from", fp, "to", tp)
 
 def main():
     height = int(input("Height of hanoi: ").strip())
-    moveTower(height, "A", "B", "C")
+    moveTower(height, "A", "C", "B")
 
 
 if __name__ == "__main__":
